0ce5b0920dbaf2c72dd2dd783734a1e56f8f435e
maisamoraes <maisa.moraes@outlook.com>
2023-08-12 15:59:15 -0300

    req2 | implementa text_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+
+    if not path_file.endswith('.txt'):
+        sys.stderr.write("Formato inválido")
+        return []
+    try:
+        with open(path_file, 'r') as file:
+            news = [linha.strip() for linha in file.readlines()]
+            return news
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+        return []
