736714632dab7515540e8e92be1ffc00d26ad253
maisamoraes <maisa.moraes@outlook.com>
2023-08-13 15:24:49 -0300

    req 7 - implementa exists_word

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,22 +3,22 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        self.__data = []
+        self._data = []
 
     def __len__(self):
-        return len(self.__data)
+        return len(self._data)
 
     def enqueue(self, value):
-        self.__data.append(value)
+        self._data.append(value)
 
     def dequeue(self):
-        if len(self.__data) == 0:
+        if len(self._data) == 0:
             return None
-        return self.__data.pop(0)
+        return self._data.pop(0)
 
     def search(self, index):
         if 0 <= index < len(self):
-            return self.__data[index]
+            return self._data[index]
         raise IndexError("Índice Inválido ou Inexistente")
 
     def empty(self):
--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,27 @@
+import re
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    search_results = []
+
+    for file in instance._data:
+        file_occurrences = [
+            {"linha": line_number}
+            for line_number, line in enumerate(
+                file["linhas_do_arquivo"], start=1
+            )
+            if re.search(re.escape(word), line, re.I)
+        ]
+
+        if file_occurrences:
+            file_info = {
+                "palavra": word,
+                "arquivo": file["nome_do_arquivo"],
+                "ocorrencias": file_occurrences,
+            }
+            search_results.append(file_info)
+
+    return search_results
 
 
 def search_by_word(word, instance):
